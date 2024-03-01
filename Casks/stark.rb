c0dba82737fd384560d0149da3b09ae3c54a2e20
Tom Bell <tomb@tomb.io>
2024-03-01 11:06:23 +0000

    Update stark cask after auditing

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -1,10 +1,13 @@
 cask 'stark' do
+  name 'Stark'
+  desc 'Power your window management with JavaScript.'
+  homepage 'https://github.com/starkwm/stark'
+
   version '2.5.0'
+  url 'https://stark-builds.s3.amazonaws.com/stark-2.5.0.zip', verified: 'https://stark-builds.s3.amazonaws.com'
   sha256 '8fdb44b29eddf79e288c5a813bd03877b435447286e94cd1ffbed4147f1ad9d7'
 
-  url 'https://s3.amazonaws.com/stark-builds/stark-2.5.0.zip'
-  name 'Stark'
-  homepage 'https://github.com/starkwm/stark'
+  depends_on macos: :sonoma
 
   app 'Stark.app'
 
