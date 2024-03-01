30f0d7717f38b74629953c505c0d5a659a1c3853
Tom Bell <tomb@tomb.io>
2024-03-01 10:58:18 +0000

    Fix audit issues

--- Formula/skbd.rb
+++ Formula/skbd.rb
@@ -8,8 +8,8 @@ class Skbd < Formula
   bottle do
     root_url "https://skbd-builds.s3.amazonaws.com"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, sonoma: "277a60e386d6b2af7718f94d79278c9ca28f9ab3b15d4b13ec25c346cef457a7"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "0aad7b1d4dc7d8ede56bec1818910ba79ecac3d18db3e77643559a8314d6c650"
+    sha256 cellar: :any_skip_relocation, sonoma:       "277a60e386d6b2af7718f94d79278c9ca28f9ab3b15d4b13ec25c346cef457a7"
   end
 
   depends_on xcode: :build
