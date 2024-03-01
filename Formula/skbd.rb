25bc596f284b728926b6e9cae4bc0b97e05170a0
Tom Bell <tomb@tomb.io>
2024-03-01 10:53:10 +0000

    Update skbd formula with arm64 build

--- Formula/skbd.rb
+++ Formula/skbd.rb
@@ -1,6 +1,7 @@
 class Skbd < Formula
   desc "Stark Keybind Daemon"
-  homepage "https://usestark.app"
+  homepage "https://github.com/starkwm/skbd"
+
   url "https://github.com/starkwm/skbd.git", tag: "v0.0.2", revision: "14664e07c75e5859d5349261d8d1c59bc893ae86"
   head "https://github.com/starkwm/skbd.git", branch: "main"
 
@@ -8,6 +9,7 @@ class Skbd < Formula
     root_url "https://skbd-builds.s3.amazonaws.com"
     rebuild 1
     sha256 cellar: :any_skip_relocation, sonoma: "277a60e386d6b2af7718f94d79278c9ca28f9ab3b15d4b13ec25c346cef457a7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0aad7b1d4dc7d8ede56bec1818910ba79ecac3d18db3e77643559a8314d6c650"
   end
 
   depends_on xcode: :build
