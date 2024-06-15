2ddb2d858fc883f14da09b9259dbee712ca70a53
Tom Bell <tomb@tomb.io>
2024-06-15 16:51:03 +0100

    Update skbd to v0.0.3

--- Formula/skbd.rb
+++ Formula/skbd.rb
@@ -1,14 +1,13 @@
 class Skbd < Formula
   desc "Stark Keybind Daemon for macOS, bind keys to shell commands"
   homepage "https://github.com/starkwm/skbd"
-  url "https://github.com/starkwm/skbd.git", tag: "v0.0.2", revision: "14664e07c75e5859d5349261d8d1c59bc893ae86"
+  url "https://github.com/starkwm/skbd.git", tag: "v0.0.3", revision: "12e296cab7cf9bea778fbcc39813719ca916e5e1"
   head "https://github.com/starkwm/skbd.git", branch: "main"
 
   bottle do
     root_url "https://skbd-builds.s3.amazonaws.com"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0aad7b1d4dc7d8ede56bec1818910ba79ecac3d18db3e77643559a8314d6c650"
-    sha256 cellar: :any_skip_relocation, sonoma:       "277a60e386d6b2af7718f94d79278c9ca28f9ab3b15d4b13ec25c346cef457a7"
+    sha256 cellar: :any_skip_relocation, sonoma: "94a5e73750acd522d9c3da69fc22a703abeee41c81be8f09aeac3b79e232b504"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "39e5ae8168c92f9cac1f12f14b04313080dd2c7062eb1b680c28139988906502"
   end
 
   depends_on xcode: :build
@@ -22,8 +21,8 @@ class Skbd < Formula
   service do
     run opt_bin/"skbd"
     keep_alive true
-    log_path var/"log/skbd.log"
-    error_log_path var/"log/skbd.log"
+    log_path var/"log/skbd/out.log"
+    error_log_path var/"log/skbd/err.log"
     environment_variables PATH: std_service_path_env
   end
 
