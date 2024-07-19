95060cdedf9a43e5b80e3f188bd1fd7d1c88237e
Tom Bell <tomb@tomb.io>
2024-07-19 23:37:48 +0100

    Update skbd to v0.0.4

--- Formula/skbd.rb
+++ Formula/skbd.rb
@@ -1,13 +1,12 @@
 class Skbd < Formula
   desc "Stark Keybind Daemon for macOS, bind keys to shell commands"
   homepage "https://github.com/starkwm/skbd"
-  url "https://github.com/starkwm/skbd.git", tag: "v0.0.3", revision: "12e296cab7cf9bea778fbcc39813719ca916e5e1"
+  url "https://github.com/starkwm/skbd.git", tag: "v0.0.4", revision: "ae492af5a5ab6210040b3929bad99aa42c2ba002"
   head "https://github.com/starkwm/skbd.git", branch: "main"
 
   bottle do
     root_url "https://skbd-builds.s3.amazonaws.com"
-    sha256 cellar: :any_skip_relocation, sonoma: "94a5e73750acd522d9c3da69fc22a703abeee41c81be8f09aeac3b79e232b504"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "39e5ae8168c92f9cac1f12f14b04313080dd2c7062eb1b680c28139988906502"
+    sha256 cellar: :any_skip_relocation, sonoma: "bab350a352a3c53f5a2d2b6c6dc7022ed3dcec22d09bb19a424f3d9f0de420e1"
   end
 
   depends_on xcode: :build
