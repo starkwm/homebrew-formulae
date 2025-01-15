07d55bd21f920b392c0c04e4f277648af3b469b8
Tom Bell <tomb@tombell.dev>
2025-01-15 23:41:14 +0000

    Rename cask to stark@tip

--- Casks/stark@tip.rb
+++ Casks/stark@tip.rb
@@ -1,8 +1,8 @@
-cask "stark@beta" do
+cask "stark@tip" do
   version :latest
   sha256 :no_check
 
-  url "https://stark-builds.s3.amazonaws.com/stark-beta.zip", verified: "stark-builds.s3.amazonaws.com/"
+  url "https://stark-builds.s3.amazonaws.com/stark-tip.zip", verified: "stark-builds.s3.amazonaws.com/"
   name "Stark"
   desc "Power your window management with JavaScript"
   homepage "https://github.com/starkwm/stark"
