68a8d2b5c9792709c4c14231ec1f6134f0824701
Tom Bell <tomb@tombell.dev>
2025-01-16 21:06:15 +0000

    Update AWS bucket names

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -2,7 +2,7 @@ cask "stark" do
   version "2.5.0"
   sha256 "8fdb44b29eddf79e288c5a813bd03877b435447286e94cd1ffbed4147f1ad9d7"
 
-  url "https://stark-builds.s3.amazonaws.com/stark-2.5.0.zip", verified: "stark-builds.s3.amazonaws.com/"
+  url "https://starkwm-builds.s3.amazonaws.com/stark-2.5.0.zip", verified: "stark-builds.s3.amazonaws.com/"
   name "Stark"
   desc "Power your window management with JavaScript"
   homepage "https://github.com/starkwm/stark"
--- Casks/stark@tip.rb
+++ Casks/stark@tip.rb
@@ -2,7 +2,7 @@ cask "stark@tip" do
   version :latest
   sha256 :no_check
 
-  url "https://stark-builds.s3.amazonaws.com/stark-tip.zip", verified: "stark-builds.s3.amazonaws.com/"
+  url "https://starkwm-builds.s3.amazonaws.com/stark-tip.zip", verified: "stark-builds.s3.amazonaws.com/"
   name "Stark"
   desc "Power your window management with JavaScript"
   homepage "https://github.com/starkwm/stark"
