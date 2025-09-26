4024b644f8c659cab2a59d31582d41eaace095fe
Tom Bell <tomb@tombell.dev>
2025-09-26 19:58:45 +0100

    Update stark to 3.0.0

--- Casks/stark.rb
+++ Casks/stark.rb
@@ -1,8 +1,8 @@
 cask "stark" do
-  version "2.5.0"
-  sha256 "8fdb44b29eddf79e288c5a813bd03877b435447286e94cd1ffbed4147f1ad9d7"
+  version "3.0.0"
+  sha256 "a7394f3a4f79b536f5e96056d719b7179b88fa9765fa07b760e31b7a9fbd422e"
 
-  url "https://starkwm-builds.s3.amazonaws.com/stark-2.5.0.zip", verified: "stark-builds.s3.amazonaws.com/"
+  url "https://starkwm-builds.s3.amazonaws.com/stark-3.0.0.zip", verified: "stark-builds.s3.amazonaws.com/"
   name "Stark"
   desc "Power your window management with JavaScript"
   homepage "https://github.com/starkwm/stark"
