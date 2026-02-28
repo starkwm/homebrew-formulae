2de20c9921d98979750112657ddee542c1ec19fb
Tom Bell <tomb@tombell.dev>
2026-02-28 12:16:32 +0000

    Add skbd@2 formula

---
+++ Formula/skbd@2.rb
@@ -0,0 +1,34 @@
+class SkbdAT2 < Formula
+  desc 'Stark Keybind Daemon for macOS, bind shortcuts to shell commands'
+  homepage 'https://github.com/starkwm/skbd.next'
+  url 'https://github.com/starkwm/skbd.next.git', tag: 'v0.0.1', revision: '3565829c10acc92ff9e917d8d927a2d70517a322'
+  head 'https://github.com/starkwm/skbd.next.git', branch: 'main'
+
+  bottle do
+    root_url 'https://starkwm-builds.s3.amazonaws.com'
+    # rebuild 1
+    # sha256 cellar: :any_skip_relocation, arm64_tahoe: 'f67742591f4f11e3f80a72d537bbcd73ae49a19ff0278e39014c718d58d3a893'
+  end
+
+  depends_on xcode: :build
+
+  depends_on arch: :arm64
+  depends_on macos: :sequoia
+
+  def install
+    system 'make', 'release'
+    bin.install "#{buildpath}/.build/release/skbd"
+  end
+
+  service do
+    run opt_bin / 'skbd'
+    keep_alive true
+    log_path var / 'log/skbd.next.log'
+    error_log_path var / 'log/skbd.next.log'
+    environment_variables PATH: std_service_path_env
+  end
+
+  test do
+    assert_match "skbd #{version}", shell_output("#{bin}/skbd --version")
+  end
+end
