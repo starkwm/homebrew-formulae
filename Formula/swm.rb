b299cd39e673656ff9885acc6fe1ad04a792825c
Tom Bell <tomb@tombell.dev>
2026-05-19 22:46:03 +0100

    Update swm bottle to v0.0.8

--- Formula/swm.rb
+++ Formula/swm.rb
@@ -8,8 +8,8 @@ class Swm < Formula
 
   bottle do
     root_url 'https://starkwm-builds.s3.amazonaws.com'
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: '3a9ab63c2fd9fed0a890886d4272ad777ca0edc810651dbf9e6aaf764fe23134'
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: '87e556721401cc2b1cc8d61f336c83b0bc3a1b165466379d25ad203fbe8052b1'
   end
 
   depends_on xcode: :build
