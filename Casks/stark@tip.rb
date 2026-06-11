a08a9c4913715668cb3047cd88fce114cf4c4176
Tom Bell <tomb@tombell.dev>
2026-06-11 18:07:47 +0100

    Update formula

--- Casks/stark@tip.rb
+++ Casks/stark@tip.rb
@@ -1,19 +1,19 @@
-cask "stark@tip" do
+cask 'stark@tip' do
   version :latest
   sha256 :no_check
 
-  url "https://starkwm-builds.s3.amazonaws.com/stark-tip.zip", verified: "starkwm-builds.s3.amazonaws.com/"
-  name "Stark"
-  desc "Power your window management with JavaScript"
-  homepage "https://github.com/starkwm/stark"
+  url 'https://starkwm-builds.s3.amazonaws.com/stark-tip.zip', verified: 'starkwm-builds.s3.amazonaws.com/'
+  name 'Stark'
+  desc 'Power your window management with JavaScript'
+  homepage 'https://github.com/starkwm/stark'
 
-  conflicts_with cask: ["stark"]
-  depends_on macos: ">= :sequoia", arch: :arm64
+  conflicts_with cask: ['stark']
+  depends_on macos: :sequoia, arch: :arm64
 
-  app "Stark.app"
+  app 'Stark.app'
 
   zap delete: [
-    "~/Library/LaunchAgents/dev.tombell.Stark.plist",
-    "~/Library/Preferences/dev.tombell.Stark.plist",
+    '~/Library/LaunchAgents/dev.tombell.Stark.plist',
+    '~/Library/Preferences/dev.tombell.Stark.plist'
   ]
 end
