369a806bebe6c7e5be1b942ba00e03d8d6baff34
Tom Bell <tomb@tombell.dev>
2026-09-10 21:49:56 +0100

    Update swm to v0.0.17

--- Formula/swm.rb
+++ Formula/swm.rb
@@ -1,15 +1,15 @@
 class Swm < Formula
-  desc "Stark Window Manager"
-  homepage "https://github.com/starkwm/swm"
+  desc 'Stark Window Manager'
+  homepage 'https://github.com/starkwm/swm'
 
-  url "https://github.com/starkwm/swm/archive/refs/tags/v0.0.16.tar.gz"
-  sha256 "ca348bd34b26b5fc406b7f7359c47b5f2e76229aec9c94da6e2a9b8e26d66e94"
-  head "https://github.com/starkwm/swm.git", branch: "main"
+  url 'https://github.com/starkwm/swm/archive/refs/tags/v0.0.17.tar.gz'
+  sha256 '71d521545e8eaeb54a959dbb96f50cacb48a8b93ea409c0af7251249b8e6839e'
+  head 'https://github.com/starkwm/swm.git', branch: 'main'
 
   bottle do
-    root_url "https://starkwm-builds.s3.amazonaws.com"
+    root_url 'https://starkwm-builds.s3.amazonaws.com'
     rebuild 7
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "14c2ed09eea10968f5a2e28f60a7ecebc0951fdad4c2986f5c45f680ce79d2fa"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: '14c2ed09eea10968f5a2e28f60a7ecebc0951fdad4c2986f5c45f680ce79d2fa'
   end
 
   depends_on xcode: :build
@@ -17,16 +17,16 @@ class Swm < Formula
   depends_on macos: :tahoe
 
   def install
-    system "make", "release"
+    system 'make', 'release'
     bin.install "#{buildpath}/.build/release/swm"
-    generate_completions_from_executable bin / "swm", "--generate-completion-script"
+    generate_completions_from_executable bin / 'swm', '--generate-completion-script'
   end
 
   service do
-    run opt_bin / "swm"
+    run opt_bin / 'swm'
     keep_alive true
-    log_path var / "log/swm.log"
-    error_log_path var / "log/swm.log"
+    log_path var / 'log/swm.log'
+    error_log_path var / 'log/swm.log'
     environment_variables PATH: std_service_path_env
   end
 
